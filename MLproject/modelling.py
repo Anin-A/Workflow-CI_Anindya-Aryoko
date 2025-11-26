a1d9573cf8048d31d187212325668df071a6323c
Anin-A <anindyaaryoko24@gmail.com>
2025-11-27 06:47:49 +0700

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -14,19 +14,28 @@ from sklearn.metrics import accuracy_score
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(42)
-    
-    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "data_preprocessing/loan_approval_dataset_preprocessing.csv")
-    df = pd.read_csv(file_path)
 
+    # Ambil argumen dengan aman
+    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+    learning_rate = float(sys.argv[2]) if len(sys.argv) > 2 else 0.1
+    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "data_preprocessing/loan_approval_dataset_preprocessing.csv"
+    )
+
+    # Load dataset
+    df = pd.read_csv(file_path)
     X = df.drop(columns=['loan_approved'])
     y = df['loan_approved']
 
+    # Oversampling dengan SMOTE
     smote = SMOTE(random_state=42)
     X_resampled, y_resampled = smote.fit_resample(X, y)
 
     print("Distribusi target setelah SMOTE:")
     print(y_resampled.value_counts(normalize=True))
 
+    # Split train-test
     X_train, X_test, y_train, y_test = train_test_split(
         X_resampled, y_resampled,
         test_size=0.2,
@@ -42,12 +51,12 @@ if __name__ == "__main__":
 
     input_example = X_train.iloc[0:5]
 
-    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
-    learning_rate = float(sys.argv[2]) if len(sys.argv) > 2 else 0.1
-
+    # Autolog MLflow
     mlflow.sklearn.autolog()
 
-    with mlflow.start_run():
+    mlflow.end_run()
+
+    with mlflow.start_run(run_name="GradientBoosting", nested=True):
         model = GradientBoostingClassifier(
             n_estimators=n_estimators,
             learning_rate=learning_rate,
@@ -57,12 +66,14 @@ if __name__ == "__main__":
 
         y_pred = model.predict(X_test)
 
+        # Log model ke MLflow
         mlflow.sklearn.log_model(
             sk_model=model,
             artifact_path="model",
             input_example=input_example
         )
 
+        # Log metric akurasi
         acc = accuracy_score(y_test, y_pred)
         print("Accuracy:", acc)
         mlflow.log_metric("accuracy", acc)
